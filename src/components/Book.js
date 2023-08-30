24f5bdac39afb780d7640ad486f9f0a6f7ee51fc
Agustin Soria <soriaagustin8@gmail.com>
2023-08-30 18:15:29 -0300

    adding action handleRemove

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,23 @@
+/* eslint-disable camelcase */
 /* eslint-disable import/no-extraneous-dependencies */
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-function Book({ title, author }) {
+function Book({ title, author, item_id }) {
+  const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    dispatch(removeBook(item_id));
+  };
+
   return (
     <div>
       <div>
         <p>{title}</p>
         <p>{author}</p>
       </div>
-      <button type="button">Remove</button>
+      <button type="button" onClick={handleRemove}>Remove</button>
     </div>
   );
 }
@@ -16,6 +25,7 @@ function Book({ title, author }) {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  item_id: PropTypes.string.isRequired,
 };
 
 export default Book;
