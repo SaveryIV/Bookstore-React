81b38d87dde4e7629e4736a21ee87e98dbfdfc23
Agustin Soria <soriaagustin8@gmail.com>
2023-08-30 18:16:25 -0300

    importing ShowBooks and AddBook in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,14 @@
+import ShowBook from './ShowBook';
+import AddBook from './AddBook';
+
 function Home() {
-  return (<><h1>Hello from Home!</h1></>);
+  return (
+    <>
+      <h1>Hello from Home!</h1>
+      <ShowBook />
+      <AddBook />
+    </>
+  );
 }
 
 export default Home;
