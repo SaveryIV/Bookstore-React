16c000ed6102ff689a0dfcd4837a487e2146a665
Agustin Soria <soriaagustin8@gmail.com>
2023-09-01 02:10:06 -0300

    Update Book component to match fetch actions

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,34 +1,57 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import { deleteBookFromApi } from '../redux/books/booksSlice';
 
-const Book = ({ title, author, item_id: ïtemId }) => {
+const Book = ({ book }) => {
   const dispatch = useDispatch();
-
-  const handleRemove = (e) => {
-    e.prevetDefault();
-    dispatch(removeBook(ïtemId));
-  };
 
   return (
     <div>
-      <form onSubmit={handleRemove}>
-        <div>
-          <p>{title}</p>
-          <p>{author}</p>
-        </div>
-        <button type="submit">
-          Remove
+      <div key={book.id}>
+        <p>{book.category}</p>
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <ul>
+          <li>
+            <button type="button">
+              Comments
+            </button>
+          </li>
+          <li>
+            <button
+              type="button"
+              onClick={() => {
+                dispatch(deleteBookFromApi(book.id));
+              }}
+            >
+              Remove
+            </button>
+          </li>
+          <li>
+            <button type="button">
+              Edit
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>CURRENT CHAPTER</p>
+        <p>Chapter 10</p>
+        <button type="button">
+          UPDATE PROGRESS
         </button>
-      </form>
+      </div>
     </div>
   );
 };
 
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  item_id: PropTypes.string.isRequired,
+  book: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    author: PropTypes.string,
+    category: PropTypes.string,
+  }).isRequired,
 };
 
 export default Book;
